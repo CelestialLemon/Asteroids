1eceb0b4dd81a7c2b3405a4ac971d23d12bbc4c2
detonator22 <ashutoshinde23@gmail.com>
2022-08-31 18:31:31 +0530

    added warping, updated the spaceship sprite to be 64x64 instead of 32x32 and using scaling

--- Asteroids/src/Spaceship.cpp
+++ Asteroids/src/Spaceship.cpp
@@ -11,12 +11,34 @@ void Spaceship::updatePosition(float dt) {
     sf::Vector2f currentVelocity = m_rigidbody.GetVelocity();
 
     float currentSpeed = sqrtf((currentVelocity.x * currentVelocity.x) + (currentVelocity.y * currentVelocity.y));
-    printf("Speed: %.2f\n", currentSpeed);
 
     float dy = currentVelocity.y * dt;
     float dx = currentVelocity.x * dt;
 
     Move(sf::Vector2f(dx, dy));
+
+
+    // warp check
+    auto currentPosition = getPosition();
+
+    // the currentPosition is of the top-left corner of sprite
+    // adjust position to center of the spaceship sprite, so it warps only when the center of sprite moves offscreen
+    // use the position of this center to calculate warping
+    currentPosition.x += 32.0f;
+    currentPosition.y += 32.0f;
+
+    // 720.0f is the height and width of screen in pixels
+    currentPosition.x = currentPosition.x < 0.0f ? 720.0f : currentPosition.x;
+    currentPosition.x = currentPosition.x > 720.0f ? 0.0f : currentPosition.x;
+
+    currentPosition.y = currentPosition.y < 0.0f ? 720.0f : currentPosition.y;
+    currentPosition.y = currentPosition.y > 720.0f ? 0.0f : currentPosition.y;
+
+    // cancel the adjustment applied earlier to return to normal
+    currentPosition.x -= 32.0f;
+    currentPosition.y -= 32.0f;
+
+    setPosition(currentPosition);
 }
 
 // take input to move the ship
