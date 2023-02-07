a4a34321d1524d2726a1ec6bc94d14b7b3354a9f
detonator22 <ashutoshinde23@gmail.com>
2023-02-07 17:49:16 +0530

    added settings menu, read-write data through json file

--- Asteroids/src/Application.h
+++ Asteroids/src/Application.h
@@ -1,13 +1,15 @@
 #pragma once
 
 #include "SFML/Graphics.hpp"
+#include "json.hpp"
 
 // switch to undefined scene to close game
 enum Scene {
     UNDEFINED_SCENE = -1,
     START_MENU_SCENE = 0,
     GAMEPLAY_SCENE = 1,
-    GAME_OVER_SCENE = 2
+    GAME_OVER_SCENE = 2,
+    SETTINGS_SCENE = 3
 };
 
 class Application{
@@ -21,6 +23,21 @@ class Application{
 
     // keep track of current score
     size_t score;
+
+    // controls overall volume of the application, has value between 0 and 1
+    float MASTER_VOLUME;
+    // controls volume of sound effect like explosins, deaths, guns etc
+    float SFX_VOLUME;
+    // controls volume of bgm
+    float MUSIC_VOLUME;
+
+    // reads game data from file
+    void loadData();
+    // saves current values of attributes in json file
+    void saveData();
+    // resets attribute values to default
+    void resetData();
+
 public:
     Application(int resX, int resY);
     void Run();
@@ -28,4 +45,5 @@ public:
     Scene StartMenuScene();
     Scene GameplayScene();
     Scene GameOverScene();
+    Scene SettingsScene();
 };
